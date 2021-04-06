b481e470d65a646ad7d476856678cf53360d4583
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-05 20:25:13 -0400

    wrote delete request

--- api/server.js
+++ api/server.js
@@ -82,5 +82,21 @@ server.put("/api/users/:id", (req, res)=>{
 
 })
 
+//[DELETE] Specific User
+
+server.delete("/api/users/:id", (req, res)=>{
+
+    const { id }=req.params;
+
+    User.remove(id)
+    .then((removedUser)=>{
+        console.log(removedUser);
+        res.status(200).json(removedUser);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message});
+    })
+})
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
