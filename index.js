bbcbdb9117c8912c1da3ebfeb9024094904694e0
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-03 13:27:02 -0400

    setup index js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const server = require('./api/server');
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-// START YOUR SERVER HERE
+server.listen(PORT, ()=>{
+    console.log(`Server Listening On Port ${PORT}`);
+});
