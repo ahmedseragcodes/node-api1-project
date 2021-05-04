22aa87bca10ed3c4166991e47fa7b60d46bd3cb5
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-03 20:11:26 -0400

    got jest working, deployed monolith and displaying users from backend on frontend

--- users-frontend/src/App.js
+++ users-frontend/src/App.js
@@ -11,15 +11,26 @@ useEffect(()=>{
   axios.get("http://localhost:5678/api/users")
   .then((res)=>{
     console.log("SUCCEEDED GETTING USERS", res);
+    setUsers(res.data);
   })
   .catch((err)=>{
     console.log("FAILED TO GET USERS", err);
   })
-})
+},[])
 
   return (
     <div>
       <h1>Users</h1>
+      {
+        users.map((user)=>{
+          return (
+            <div>
+              <p>Name: {user.name}</p>
+              <p>Bio: {user.bio}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
